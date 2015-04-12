1d5e8537087e9dbc08cb6329d6afd5cca139e5b9
Michael Hartigan <e3ae85d6@gmail.com>
2015-04-12 18:18:28 +0600

    module to read hex file added

--- hexfile.js
+++ hexfile.js
@@ -1,5 +1,5 @@
 
-var readFileByLine = require("./readFileByLine");
+var readHexFile = require("./readHexFile");
 
 var hexFilename = process.argv[2];
 if(hexFilename === undefined) {
@@ -9,54 +9,14 @@ if(hexFilename === undefined) {
 
 console.log("loading hex file: " + hexFilename);
 
-function hexfileRecordFromString(str) {
-
-	if(str[0] != ':')
-		return null;
-
-	var binRecord = new Buffer(str.substring(1), "hex");
-
-	var byteCount = binRecord[0];
-
-	var addrHi = binRecord[1];
-	var addrLo = binRecord[2];
-	var addr = addrHi << 8 | addrLo;
-
-	var recordType = binRecord[3];
-	
-	var data = binRecord.slice(4, -1);
-	if(data.length != byteCount)
-		return null;
-
-	var checksum = binRecord[binRecord.length - 1];
-	checksum += (byteCount + addrHi + addrLo + recordType);
-	for (var i = 0; i < data.length; i++)
-		checksum += data[i];
-
-	checksum &= 0xFF;
-	if(checksum != 0)
-		return null;
-
-	return {
-		"recordType" : recordType,
-		"addr" : addr,
-		"data" : data,
-	};
-}
-
-readFileByLine(hexFilename, function(err, line) {
+readHexFile(hexFilename, function(err, hexfileRecord) {
 	if(err) throw err;
 
-	if(line) {
-		var hexFileRecord = hexfileRecordFromString(line);
-		if(hexFileRecord != null) {
-			console.log(
-				"recordType: " + hexFileRecord.recordType +
-				"; addr: " + hexFileRecord.addr +
-				"; data: " + hexFileRecord.data.toString("hex"));
-		}
+	if(hexfileRecord != null) {
+		console.log("recordType: " + hexfileRecord.recordType + "; addr: " + hexfileRecord.addr + "; data: " + hexfileRecord.data.toString("hex"));
 	}
-	else
-		console.log("done!!!");
+	else {
+		console.log("done");
+	}
 });
 
